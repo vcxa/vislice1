bbcaa9e4fc8a309c4283f36adc853f345e28696a
vcxa <49915149+vcxa@users.noreply.github.com>
2019-05-27 11:53:57 +0200

    Update tekstovni_vmesnik.py

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,20 +1,20 @@
 import model
 
 def izpis_igre(igra):
-    return """=========================
-    {geslo}
-    Napačne črke: {napacne_crke}
-    Ugibaš še {stevilo}-krat.
-    =======================""".format(
-        geslo=igra.pravilni_del_gesla(),
-        napacne_crke=igra.nepravilni_ugibi(),
-        stevilo=model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak())
+    return """==================================
+{geslo}
+Napačne črke: {napacne_crke}
+Ugibaš še {stevilo}-krat.
+=====================================""".format(
+    geslo=igra.pravilni_del_gesla(),
+    napacne_crke=igra.nepravilni_ugibi(),
+    stevilo=model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak())
 
 def izpis_zmage(igra):
     return "Čestitke! Uganil/a si geslo {}.".format(igra.geslo)
 
 def izpis_poraza(igra):
-    return "LUZERR!! Pravilno geslo je bilo:{}".format(igra.geslo)
+    return "Jebote si glup! Pravilen odg: {}".format(igra.geslo)
 
 def zahtevaj_vnos():
     return input("ugibaj: ")
